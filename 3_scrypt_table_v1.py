ddb0e036a026d62e2c78a68b851dc64c5c7c29b7
qqbek24 <qqbek24@gmail.com>
2020-09-10 15:08:14 +0200

    code cleaning

---
+++ 3_scrypt_table_v1.py
@@ -0,0 +1,11 @@
+#****_tabelka
+#****_Autor: Jakub Koziorowski
+
+imie = "Jakub"
+mail = "Jakub123@gmail.com"
+NrStopy = 43
+imie2 = "Kasia"
+mail2 = "Kasia123@gmail.com"
+NrStopy2 = 36
+print(imie, "\t", mail, "\t", NrStopy, "\n", imie2, "\t", mail2, "\t", NrStopy2)
+
