99b57b5ed1bbaeee01b76c39f413a3ca2c066f1c
qqbek24 <qqbek24@gmail.com>
2020-09-10 14:08:10 +0200

    code "cleaning"

--- home_work_Piramidka _z_liter.py
+++ home_work_Piramidka _z_liter.py
@@ -3,14 +3,14 @@
 
 import string
 
-number=int(input("Podaj liczbe: "))
-spaces=2*number-2
-for x in range(0,number):
-    for y in range(0,spaces):
+number = int(input("Input a number: "))
+spaces = 2*number-2
+for x in range(0, number):
+    for y in range(0, spaces):
         print(end=" ")
-    spaces=spaces-1
-    for y in range(0,x+1):
+    spaces = spaces-1
+    for y in range(0, x+1):
         print(string.ascii_lowercase[y], end="")
-    for y in range(x,0,-1):
+    for y in range(x, 0, -1):
         print(string.ascii_lowercase[y-1], end="")
     print("\n")
