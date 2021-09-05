9b2ca3812f7b203ce853d691ef768e1b304a660f
qqbek24 <70805462+qqbek24@users.noreply.github.com>
2021-09-05 20:15:53 +0200

    Create Palindrom_TDD.py

---
+++ Palindrom_TDD.py
@@ -0,0 +1,11 @@
+#****_Palindrom TDD examples
+#****_Autor: Jakub Koziorowski
+
+class Palindrom:
+    def check_validity(self, text):
+        if len(text) == 0:
+            return False
+        if len(text) == 1:
+            return True
+        text = text.upper()
+
