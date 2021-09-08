f88e1ff0d1210073002307d0b023dce27a1bcfcf
qqbek24 <70805462+qqbek24@users.noreply.github.com>
2021-09-08 23:59:44 +0200

    Update working_with_CSV_ex.py
    PEP 8

--- working_with_CSV_ex.py
+++ working_with_CSV_ex.py
@@ -4,11 +4,12 @@
 import csv
 
 moja_lista=[
-    ["Jakub","Jakub123@gmail.com",43],
-    ["Kasia","Kasia123@gmail.com",36],
-    ["Pawel","Pawel123@wp.com",48]
+    ["Jakub", "Jakub123@gmail.com", 43],
+    ["Kasia", "Kasia123@gmail.com", 36],
+    ["Pawel", "Pawel123@wp.com", 48]
 ]
+print(moja_lista)
 
-with open('dane_osobowe_csv', "wt") as f:
-    csv_out = csv.writer(f)
-    csv_out.writerows(moja_lista)
+#with open('dane_osobowe_csv', "wt") as f:
+#    csv_out = csv.writer(f)
+#    csv_out.writerows(moja_lista)
